9a91cca7f9c4fd39dfdbb25362cb7f1fbc3c2a38
GatlinThompson <112907390+GatlinThompson@users.noreply.github.com>
2025-07-20 20:15:03 -0600

    fix: add copyright symbol to footer text

--- app/ui/footer/Footer.tsx
+++ app/ui/footer/Footer.tsx
@@ -5,7 +5,8 @@ export default function Footer() {
     <footer
       className={`${styles.footer} border-t-1 border-foreground flex flex-row items-center align-center justify-center px-4 gap-4 w-full text-secondary-foreground shadow-t-4 inset-shadow-sm `}
     >
-      <p className="text-primary">Weatherly 2025</p>
+      <p className="text-primary">&copy; Weatherly 2025</p>
+      <p className="text-primary">Powered by Tommorow.io</p>
     </footer>
   );
 }
